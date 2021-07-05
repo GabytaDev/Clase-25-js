b5cb8c0a4d0d89cbbfa7fbce1efd88252664c54b
GabytaDev <gabyta.loyola@gmail.com>
2021-07-05 16:33:17 -0300

    ejercicio 8 dias a segundos

---
+++ mumuki js8/index.js
@@ -0,0 +1,11 @@
+// Necesitamos un programa que pida una cantidad de días con el siguiente mensaje:
+
+// Ingresá una cantidad de días por favor
+// El programa deberá mostrar el resultado de la conversión de los días a segundos 
+// mediante el mensaje: El resultado de la conversión de {dias} días a segundos es: {resultado}
+
+let dias = prompt ("Ingresá una cantidad de días por favor");
+
+let resultado = dias * 86400
+
+alert ("El resultado de la conversión de " + dias + " días a segundos es: " + resultado);
